e659a3d10028af589058352debbaff5304e1c7e5
Abdoulaye PATAWALA <patawalaabdoulaye2003@gmail.com>
2025-07-17 21:30:59 +0100

    update meta tags for social preview

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,19 +1,30 @@
-// app/head.tsx
 export default function Head() {
   return (
     <>
       <title>Phythasoft - Formations pratiques et accessibles pour tous</title>
-      <meta name="description" content="Inscrivez-vous dès maintenant aux formations Phythasoft : développement web, mobile, bureautique et design graphique, même sans expérience." />
+      <meta
+        name="description"
+        content="Inscrivez-vous dès maintenant aux formations Phythasoft : développement web, mobile, bureautique et design graphique, même sans expérience."
+      />
 
-      {/* Open Graph pour Facebook */}
+      {/* Open Graph pour Facebook, WhatsApp, LinkedIn */}
       <meta property="og:title" content="🌱 Rejoignez Phythasoft et changez votre avenir !" />
-      <meta property="og:description" content="Formations pratiques en création de sites, bureautique et design, accessibles même aux débutants. Inscrivez-vous gratuitement !" />
-      <meta property="og:image" content="https://phythasoft.vercel.app/dev.png" />
-      <meta property="og:url" content="https://phythasoft.vercel.app" />
+      <meta
+        property="og:description"
+        content="Formations pratiques en création de sites, bureautique et design, accessibles même aux débutants. Inscrivez-vous gratuitement !"
+      />
+      <meta property="og:image" content="https://pythasoft.vercel.app/cover.png" />
+      <meta property="og:url" content="https://pythasoft.vercel.app" />
       <meta property="og:type" content="website" />
 
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="🌱 Rejoignez Phythasoft et changez votre avenir !" />
+      <meta
+        name="twitter:description"
+        content="Formations pratiques en création de sites, bureautique et design, accessibles même aux débutants. Inscrivez-vous gratuitement !"
+      />
+      <meta name="twitter:image" content="https://pythasoft.vercel.app/cover.png" />
     </>
-  )
+  );
 }
